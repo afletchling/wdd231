edb323ec172b371f2f5ea6b76e264c8cd3d4fb40
afletchling <240793075+afletchling@users.noreply.github.com>
2026-03-26 14:14:40 -0600

    W03: Fixed Weather API

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -67,68 +67,20 @@ if (businessHolder) {
 if (weatherData) {
     const URL = 'https://api.openweathermap.org/data/2.5';
     async function fillData() {
-        let data = await fetch(`${URL}/forecast/daily?lat=49.695087&lon=-112.845262&cnt=4&units=imperial&appid=85fd394a0405664633b0796bead3e79e`);
+        let data = await fetch(`${URL}/forecast?lat=49.695087&lon=-112.845262&cnt=4&units=imperial&appid=85fd394a0405664633b0796bead3e79e`);
         if (data.ok) {
             data = await data.json();
         } else {
             console.log(`Failed to fetch data: ${await data.text()}`);
-            data = { // placeholder data since the API key is not active currently.
-                list: [
-                    {
-                        dt: 1774001448,
-                        weather: {
-                            description: "light rain"
-                        },
-                        temp: {
-                            "day": 16,
-                            "min": 10,
-                            "max": 18
-                        }
-                    },
-                    {
-                        dt: 1774087848,
-                        weather: {
-                            description: "moderate rain"
-                        },
-                        temp: {
-                            "day": 12,
-                            "min": 5,
-                            "max": 14
-                        }
-                    },
-                    {
-                        dt: 1774174248,
-                        weather: {
-                            description: "heavy rain"
-                        },
-                        temp: {
-                            "day": 6,
-                            "min": -1,
-                            "max": 8
-                        }
-                    },
-                    {
-                        dt: 1774260648,
-                        weather: {
-                            description: "sunny"
-                        },
-                        temp: {
-                            "day": 23,
-                            "min": 17,
-                            "max": 27
-                        }
-                    }
-                ]
-            };
         }
 
         const currentDay = data.list.splice(0, 1)[0];
-        const desc = currentDay.weather.description;
+        const desc = currentDay.weather[0].description;
         weatherData.innerHTML = [
-            `<span class="bold">${currentDay.temp.day}C</span>`,
+            `<span class="bold">${currentDay.main.temp.toFixed(0)}C</span>`,
             desc.split(' ').map((word) => word.charAt(0).toLocaleUpperCase() + word.substring(1)).join(' '),
             ...data.list.map((day) => {
-                return `${new Date(day.dt * 1000).toLocaleDateString('en-us', {weekday: 'long'})}: ${day.temp.day}C`
+                return `${new Date(day.dt * 1000).toLocaleDateString('en-us', {weekday: 'long'})}: ${day.main.temp.toFixed(0)}C`
             })
         ].join('<br>');
     };
